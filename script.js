d84e82472c104f7625e935008021068cacd44f9d
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-03-25 19:05:17 +0200

    script modal1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let dialogBtn1 = document.querySelector("#dialog_btn1");
 let dialogBtn2 = document.querySelector("#dialog_btn2");
 let dialogBtn3 = document.querySelector("#dialog_btn3");
-
+let dialogCloseBtn = document.querySelector("#modal1 button");
 
 let modal = document.querySelector("#modal1");
 
@@ -9,4 +9,11 @@ let modal = document.querySelector("#modal1");
 dialogBtn1.onclick = function(event){
     event.preventDefault();
     modal.style.display = "flex";  
+}
+
+dialogCloseBtn.onclick = function(event){
+    // event.preventDefault();
+    event.target.parentElement.parentElement.style.display = "none"; 
+
+
 }
