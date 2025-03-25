f621d437a1f5cb1862600cc2e0b2ee19394e5112
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-03-25 19:22:54 +0200

    javascript modals

--- script.js
+++ script.js
@@ -5,11 +5,45 @@ let dialogCloseBtn = document.querySelector("#modal1 button");
 
 let modal = document.querySelector("#modal1");
 
+let modalData = [
+    { 
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png",
+        h3:"Kas ir HTML",
+        p:"HTML (HyperText Markup Language) ir tīmekļa lapu struktūras veidošanas valoda, kas nosaka, kā saturs tiek organizēts un attēlots pārlūkprogrammā."
+    },
+    {
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png",
+        h3:"Kas ir CSS",
+        p:"CSS (Cascading Style Sheets) tiek izmantots, lai pievienotu stilu un dizainu HTML elementiem, piemēram, mainītu krāsas, fontus un izkārtojumu."
+    },
+   {
+        img:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png",
+        h3:"Kas ir JavaScript",
+        p:"JavaScript ir programmēšanas valoda, kas pievieno interaktivitāti un dinamisku funkcionalitāti tīmekļa lapām, piemēram, formu validāciju, animācijas un lietotāju darbību apstrādi."
+    }
+]
 
 dialogBtn1.onclick = function(event){
     event.preventDefault();
+    openModal(0);
+}
+dialogBtn2.onclick = function(event){
+    event.preventDefault();
+    openModal(1);
+}
+dialogBtn3.onclick = function(event){
+    event.preventDefault();
+    openModal(2);
+}
+
+function openModal(number){
+    modal.querySelector("img").src = modalData[number].img;
+    modal.querySelector("h3").innerText = modalData[number].h3;
+    modal.querySelector("p").innerText = modalData[number].p;
     modal.style.display = "flex";  
 }
+
+
 
 dialogCloseBtn.onclick = function(event){
     // event.preventDefault();
